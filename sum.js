b5a8b909247b2011a156a4c9c7d4e0ea37bc1821
nehapansare <nehapansare007@gmail.com>
2024-06-28 12:01:59 +0530

    javascript added

---
+++ sum.js
@@ -0,0 +1,11 @@
+const inputnum1 = document.getElementById("num1");
+const inputnum2 = document.getElementById("num2");
+const answer = document.getElementById("result");
+
+function calculateSum(){
+    const val1 = parseInt(inputnum1.value);
+    const val2 = parseInt(inputnum2.value);
+    const sum = val1 + val2;
+    answer.innerText =Sum of ${val1} and ${val2} is ${sum}
+
+}
